d197d9027af3d0ad9f89d89ce007637401bf0c86
Acdar <>
2021-01-06 15:34:46 +0800

    update from gridea: 2021-01-06 15:34:46

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c775cd322537048936441fce2244264a.js"
+  "precache-manifest.4071da25fcfe75e5ce2984870be0767f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
